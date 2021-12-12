625c76641e90e37455a219decd64f41e72d5f540
Huybg234 <nqhuybg234@gmail.com>
2021-12-12 23:36:36 +0700

    huy

--- src/app/admin/profile/profile.component.ts
+++ src/app/admin/profile/profile.component.ts
@@ -1,4 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponseData } from 'src/app/model/response-data';
+import { JobRegister } from '../../model/job-register';
+import { JobRegisterService } from '../../services/job-register.service';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +30,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private JobRegisterService:JobRegisterService) { }
+  // data:any
+
+  displayedColumns: string[] = [
+    'user',
+    'vacancies',
+    'applicationTime',
+    'status',
+    'cvFile'
+  ];
+
+
+  dataSource: JobRegister[] = [];
 
   ngOnInit(): void {
+    // this.loaddata();
+    this.JobRegisterService.getAllJobregister().subscribe(data => {
+      debugger;
+      this.dataSource = data;
+      console.log(this.dataSource);
+
+    })
   }
 
 }
